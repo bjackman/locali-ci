3d5646d83e8a85e34de9a3c3d52dbe002bbd7df8
Brendan Jackman <jackmanb@chromium.org>
2024-07-12 22:19:52 +0200

    Remove -path prefix from arg names
    Too verbose

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,18 @@ struct Args {
     // TODO: Don't require valid utf-8 strings here, OsStrings shoud be fine. But
     // https://stackoverflow.com/questions/76341332/clap-default-value-for-pathbuf
     #[arg(short, long, default_value_t = {".".to_string()})]
-    repo_path: String,
+    repo: String,
     /// Maximum number of tests to run concurrently. Each concurrent thread
     /// requires creating a worktree, which is why we don't default to $nproc.
     #[arg(short, long, default_value_t = 8)]
     num_threads: u32,
+    /// Command to test. Note this is _not_ run via the shell.
+    #[arg(short, long, required = true)]
+    config: PathBuf,
     /// Base of range to test. Will test commits between this (exclusive) and
     /// HEAD (inclusive). Whenever HEAD changes, this string will be re-evaluated
     /// to find the base of the range.
     base: String,
-    /// Command to test. Note this is _not_ run via the shell.
-    #[arg(short, long, required = true)]
-    config_path: PathBuf,
 }
 
 #[tokio::main]
@@ -43,14 +43,14 @@ async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let repo = git::PersistentWorktree {
-        path: args.repo_path.to_owned().into(),
+        path: args.repo.to_owned().into(),
     };
     // Check repo is valid.
     repo.git_dir()
         .await
-        .context(format!("opening repo {}", args.repo_path))?;
+        .context(format!("opening repo {}", args.repo))?;
     let repo = Arc::new(repo);
-    let mut m = config::create_manager(repo.clone(), &args.config_path).await?;
+    let mut m = config::create_manager(repo.clone(), &args.config).await?;
     let range_spec: OsString = format!("{}..HEAD", args.base).into();
     m.set_revisions(repo.rev_list(&range_spec).await.context("couldn't rev-list")?);
     let mut revs_stream = repo.watch_refs(&range_spec)?;
