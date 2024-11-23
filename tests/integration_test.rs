175cc2fdfa853c890c195569dfa001cec8307cd9
Brendan Jackman <jackmanb@chromium.org>
2024-11-23 19:50:06 +0100

    Revert "Embiggen integration test timeouts"
    This reverts commit 03e1aed3a2f0370fde87b756bbc8a3b1efb31006.
    
    Wasn't a timeout thing.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -210,7 +210,7 @@ impl LimmatChild {
                     }
                 }
             },
-            Duration::from_secs(10),
+            Duration::from_secs(5),
         )
     }
 }
@@ -234,7 +234,7 @@ async fn test_worktree_teardown(test_command: &str) {
         .await
         .unwrap();
 
-    wait_for(|| limmat.has_worktrees(), Duration::from_secs(10))
+    wait_for(|| limmat.has_worktrees(), Duration::from_secs(5))
         .expect("worktree not found after 5s");
 
     limmat.terminate().expect("couldn't shut down child");
@@ -273,7 +273,7 @@ async fn shouldnt_leak_jobs() {
 
     // Wait for test to start up
     let test_pid_path = temp_dir.path().join("test_pid");
-    wait_for(|| Ok(test_pid_path.exists()), Duration::from_secs(10))
+    wait_for(|| Ok(test_pid_path.exists()), Duration::from_secs(5))
         .expect("worktree not found after 5s");
     let pid: pid_t = pid_t::from_str(fs::read_to_string(test_pid_path).unwrap().trim()).unwrap();
 
@@ -308,7 +308,7 @@ async fn should_invalidate_cache_when_dep_changes() {
             ))
             .await
             .unwrap();
-        wait_for(|| Ok(test_ran_path.exists()), Duration::from_secs(10)).expect("test not ran");
+        wait_for(|| Ok(test_ran_path.exists()), Duration::from_secs(5)).expect("test not ran");
 
         // Shut down child by dropping it. This is racy, it's possible we
         // haven't finished writing the test DB yet. In that case this test
@@ -338,6 +338,6 @@ async fn should_invalidate_cache_when_dep_changes() {
         ))
         .await
         .unwrap();
-    wait_for(|| Ok(test_ran_path.exists()), Duration::from_secs(10))
+    wait_for(|| Ok(test_ran_path.exists()), Duration::from_secs(5))
         .expect("test not re-ran when dependency config changed");
 }
